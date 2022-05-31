ddb6bfd4674d2260f1912b9f5171be31c3b624d7
Tobias Schotter <t.schotter@oth-aw.de>
2022-05-31 17:15:05 +0200

    fixed footer css, added temporary routing for editor

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditorComponent } from './editor/editor.component';
+import { ArtikelComponent } from './artikel/artikel.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: '', component: ArtikelComponent},
+  { path: 'artikel', component: ArtikelComponent},
+  { path: 'edit', component: EditorComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+export const rountingComponents = [ EditorComponent, ArtikelComponent]
--- frontend/src/app/editor/editor.component.ts
+++ frontend/src/app/editor/editor.component.ts
@@ -15,31 +15,33 @@ export class EditorComponent implements OnInit {
 
   title = 'frontend';
   editorText = '';
+  rawText = '';
 
   changedEditor(event: EditorChangeContent | EditorChangeSelection){
-    console.log('editor got changed', event);
+    console.log(event);
     this.editorText = event['editor']['root']['innerHTML'];
+    this.rawText = event['editor']['root']['innerText'];
   }
 
   editorModule = {
     toolbar: [
       ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-      ['blockquote', 'code-block'],
+      // ['blockquote', 'code-block'],
   
-      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+      [{ 'header': 1 }, { 'header': 2 }, { 'header': 3 }],               // custom button values
       [{ 'list': 'ordered'}, { 'list': 'bullet' }],
       [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-      [{ 'direction': 'rtl' }],                         // text direction
+      // [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+      // [{ 'direction': 'rtl' }],                         // text direction
   
       // [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
       [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
   
-      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      // [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
       // [{ 'font': [] }],
       [{ 'align': [] }],
   
-      ['clean'],                                         // remove formatting button
+      // ['clean'],                                         // remove formatting button
   
       // ['link', 'image', 'video']                         // link and image, video
     ]
