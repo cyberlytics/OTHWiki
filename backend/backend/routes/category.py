df68eb58e46957188c43ad827d5bd05e84c736f1
Johannes Horst <j.horst@oth-aw.de>
2022-06-02 21:16:13 +0200

    Routen und Models vorerst fertig
    routen für alle vorerst nötigen Operationen, wird noch wieter ausgebaut, aber damit kann man erstmal arbeiten.
    Models auch festgelegt.

--- backend/backend/routes/category.py
+++ backend/backend/routes/category.py
@@ -1,5 +1,6 @@
 from dataclasses import asdict
 from fastapi import APIRouter
+import pymongo
 
 from ..schemas.category import categoriesEntity, categoryEntity
 from ..models.category import Category
@@ -7,44 +8,51 @@ from ..database import conn
 
 router_category = APIRouter()
 
-@router_category.get('/categories/{name}')
+@router_category.get('/categories/{name}', tags=["Kategorien"])
 async def find_category_by_name(name: str):
     #TODO: In Object Parsen
-    x = categoryEntity(conn.local.categories.find_one({"kategorie" : name}))
+    x = conn.local.categories.find_one({"kategorie" : name}, {"_id": 0})
     return x
 
-@router_category.get('/categories/')
+@router_category.get('/categories/', response_model=list[Category], tags=["Kategorien"])
 async def find_all_categories():
-    #TODO: In Object Parsen
     x = conn.local.categories.find()
-    res = categoriesEntity(x)
-    print(res)
-    print(type(res))
-    return res
 
-@router_category.delete('/categories/{id}')
+    #list constructor frist dicts nicht list(x)
+    lst = []
+    for y in x:
+        lst.append(y)
+    return lst
+
+@router_category.delete('/categories/{id}', tags=["Kategorien"])
 async def delete_category(id: int):
-
-    #x = conn.local.categoreis.find_one({"kategorie_id" : id})
-    #if(x == None){
-    #}
     x = conn.local.categories.delete_one({"kategorie_id" : id})
     return {"delted articles" : x.deleted_count, "raw_Result" : x.raw_result}
 
 
-@router_category.post('/categories/')
+@router_category.post('/categories/', tags=["Kategorien"])
 async def create_category(body: Category):
     print(body)
     print(asdict(body))
-    #y = dict(body)
     x = conn.local.categories.insert_one(asdict(body))
     return asdict(body)
 
-@router_category.put('/categories/{id}')
-async def add_subcategory(HeaderCat : int, body: Category):
-    print("cat", HeaderCat)
-    print("body", body)
-    return {}
+@router_category.put('/categories/{id}', tags=["Kategorien"])
+async def add_subcategory(id : int, subcat: int):
+    x = conn.local.categories.find_one({"kategorie_id" : id}, {"_id" : 0})
+    print(x)
+    print("old", x["subkategorien"])
+    newsub = []
+    if(x["subkategorien"] is not None):
+        newsub = x["subkategorien"]
+        newsub.append(subcat)
+    else:
+        newsub = [subcat]
+    x = conn.local.categories.update_one(
+    {"kategorie_id" : id},
+    {"$set" : {"subkategorien" : newsub}}
+    )
+    return x.raw_result
 
 
 
--- backend/backend/schemas/category.py
+++ backend/backend/schemas/category.py
@@ -1,10 +1,14 @@
+from dataclasses import asdict
+
+
 def categoryEntity(item) -> dict:
     return{
        "id":str(item["_id"]),
         "kategorie":item["kategorie"],
         "kategorie_id" : item["kategorie_id"],
-        "subkategorien": item["subkategorien"]
+        "subkategorien": item["subkategorien"],
+        "artikel": item["artikel"],
     }
 
 def categoriesEntity(entity) -> list:
-    return [categoryEntity(item) for item in entity]
+    return [asdict(item) for item in entity]
